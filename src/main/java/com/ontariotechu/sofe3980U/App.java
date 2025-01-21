cff4a9ad032ef642378bb1815fa19ceb453e249e
Dhruvin Patel <dhruvinvpatel12@gmail.com>
2025-01-21 17:03:48 -0500

    testing cases

--- src/main/java/com/ontariotechu/sofe3980U/App.java
+++ src/main/java/com/ontariotechu/sofe3980U/App.java
@@ -1,27 +1,39 @@
 package com.ontariotechu.sofe3980U;
 
-import org.joda.time.LocalTime;
+import java.util.Scanner;
 
 /**
- * Hello world!
- *
+ * Main program
  */
 public class App {
-    /**
-     * Main program: The entry point of the program. The local time will be printed
-     * first,
-     * Then it will create two binary variables, add them and print the result.
-     *
-     * @param args: not used
-     */
     public static void main(String[] args) {
-        LocalTime currentTime = new LocalTime();
-        System.out.println("The current local time is: " + currentTime);
-        Binary binary1 = new Binary("00010001000");
-        System.out.println("First binary number is " + binary1.getValue());
-        Binary binary2 = new Binary("111000");
-        System.out.println("Second binary number is " + binary2.getValue());
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Binary Calculator!");
+
+        // Get the first binary number from the user
+        System.out.print("Enter the first binary number: ");
+        String binary1Input = scanner.nextLine();
+        Binary binary1 = new Binary(binary1Input);
+
+        // Get the second binary number from the user
+        System.out.print("Enter the second binary number: ");
+        String binary2Input = scanner.nextLine();
+        Binary binary2 = new Binary(binary2Input);
+
         Binary sum = Binary.add(binary1, binary2);
-        System.out.println("Their summation is " + sum.getValue());
+        System.out.println("Result of ADD: " + sum.getValue());
+
+        Binary andResult = Binary.and(binary1, binary2);
+        System.out.println("Result of AND: " + andResult.getValue());
+
+        Binary orResult = Binary.or(binary1, binary2);
+        System.out.println("Result of OR: " + orResult.getValue());
+
+        Binary multiplyResult = Binary.multiply(binary1, binary2);
+        System.out.println("Result of MULTIPLY: " + multiplyResult.getValue());
+
+        System.out.println("Exiting the Binary Calculator!");
+
+        scanner.close();
     }
 }
